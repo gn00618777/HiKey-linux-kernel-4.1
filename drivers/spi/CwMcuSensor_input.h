3949528bff5bd0db72b08f338babea7c378c2212
berniechen <bernie.chen@cyweemotion.com>
2017-04-19 23:20:43 -0700

    adding input device include file

---
+++ drivers/spi/CwMcuSensor_input.h
@@ -0,0 +1,28 @@
+#ifndef __CYWEE_VIRTUAL_DRIVER_H__
+#define __CYWEE_VIRTUAL_DRIVER_H__
+
+#define ACC_MAX          2000
+#define ACC_MIN         -2000
+#define GYRO_MAX         2000
+#define GYRO_MIN        -2000
+#define MAG_MAX          4096
+#define MAG_MIN         -4096
+
+
+#define MAX_TOUCH_COUNT     5
+
+
+#define ACC_NAME        "CyWee Virtual Accelerometer"
+#define GYRO_NAME       "CyWee Virtual Gyroscope"
+#define MAG_NAME        "CyWee Virtual Magnetic Field"
+#define TOUCH_NAME      "CyweeVirtualTouch"
+#define MOUSE_DEV       "virmouse"
+#define MOUSE_CR1_NAME  "CyweeVirtualMouse1"
+#define MOUSE_CR2_NAME  "CyweeVirtualMouse2"
+#define VIRKEY_NAME     "CyweeVirtualKey"
+#define FUSION_RC1_NAME "CyweeFusionRC1"
+#define FUSION_RC2_NAME "CyweeFusionRC2"
+#define GESTURE_RC1_NAME        "CyweeGestureRC1"
+#define GESTURE_RC2_NAME        "CyweeGestureRC2"
+
+#endif
